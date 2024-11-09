ce972c7857ab5ceebeb11fdbb6eb72bc253a07f9
Reminious <115909000+Reminious@users.noreply.github.com>
2024-11-09 19:17:01 +0800

    enable async and transaction lock fix
    make report and email sending async, separate createReview & updateRecipeRating to fix lock problem

--- src/main/java/nus/iss/se/team9/report_review_service/controller/ReviewController.java
+++ src/main/java/nus/iss/se/team9/report_review_service/controller/ReviewController.java
@@ -9,22 +9,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/review")
 public class ReviewController {
-    private final ReviewService reviewService;
-    private final UserService userService;
-    private final JwtService jwtService;
-
-    @Autowired
-    public ReviewController(ReviewService reviewService, UserService userService, JwtService jwtService) {
-        this.reviewService = reviewService;
-        this.userService = userService;
-        this.jwtService = jwtService;
-    }
-
-    @PostMapping("/create")
-    public ResponseEntity<String> createReview(@RequestBody Review review,@RequestHeader("Authorization") String token) {
-        Member member = userService.getMemberById(jwtService.extractId(token));
-        review.setMember(member);
-        reviewService.createReview(review);
-        return ResponseEntity.ok("Review created successfully for recipe ID: " + review.getRecipe().getId());
-    }
+	private final ReviewService reviewService;
+	private final UserService userService;
+	private final JwtService jwtService;
+	
+	@Autowired
+	public ReviewController(ReviewService reviewService, UserService userService, JwtService jwtService) {
+		this.reviewService = reviewService;
+		this.userService = userService;
+		this.jwtService = jwtService;
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<String> createReview(@RequestBody Review review,
+											   @RequestHeader("Authorization") String token) {
+		Member member = userService.getMemberById(jwtService.extractId(token));
+		review.setMember(member);
+		reviewService.createReview(review);
+		reviewService.updateRecipeRating(review);
+		return ResponseEntity.ok("Review created successfully for recipe ID: " + review.getRecipe()
+																					   .getId());
+	}
 }
